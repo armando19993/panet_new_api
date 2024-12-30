eee87f9e969a78ac8588c7d533761c0349591e5f
Ing. Armando Campos <armandocamposf@gmail.com>
2024-12-30 07:05:50 -0400

    Update Clientes

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateClientDto } from "./dto/create-client.dto";
 import { UpdateClientDto } from "./dto/update-client.dto";
 import { PrismaService } from "src/prisma/prisma.servise";
+import axios from "axios";
 
 @Injectable()
 export class ClientService {
@@ -13,7 +14,37 @@ export class ClientService {
     return { data, message: "Cliente creado con exito" };
   }
 
-  async findAll(intemediarioId) {}
+  async findAll(intemediarioId) {
+    try {
+      const response = await axios.get('https://desarrollo.paneteirl.store/api/v1/clients');
+
+      const clients = response.data.data;
+
+      clients.forEach(async (client) => {
+        await this.prisma.client.upsert({
+          where: {
+            document: client.document
+          },
+          update: {
+            phone: client.phone,
+            email: client.phone,
+            name: client.full_name
+          },
+          create: {
+            document: client.document,
+            phone: client.phone,
+            email: client.phone,
+            name: client.full_name
+          }
+        })
+      });
+
+      console.log('Procesamiento completado.');
+    } catch (error) {
+      console.error('Error al obtener o procesar los datos:', error.message);
+      throw new Error('Error al obtener los datos desde la API externa');
+    }
+  }
 
   async findOne(document) {
     const data = await this.prisma.client.findFirst({
