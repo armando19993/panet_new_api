b7b35b24aed50aac01d16206a4156ab0b039aa15
Ing. Armando Campos <armandocamposf@gmail.com>
2025-02-08 15:55:48 -0400

    fix: update

--- src/recharge/recharge.service.ts
+++ src/recharge/recharge.service.ts
@@ -58,7 +58,7 @@ export class RechargeService {
         };
 
         //Agregar currency solo si el país es PE
-        if (data.country === "PE") {
+        if (data.country == "PE") {
           payload.currency = "PEN";
         }
 
