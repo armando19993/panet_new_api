e3a341f4dce97d83ea61c71a4b43163421019632
Dev. Armando Campos <armandocamposf@gmail.com>
2025-12-16 17:21:37 -0400

    _fix: udpate

--- src/prisma/prisma.servise.ts
+++ src/prisma/prisma.servise.ts
@@ -2,7 +2,13 @@ import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit{
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        super({
+            datasourceUrl: process.env.DATABASE_URL,
+        });
+    }
+
     async onModuleInit() {
         await this.$connect()
     }
