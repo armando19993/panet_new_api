388bdbd241dee1353806b3a55a802171803e0686
Dev. Armando Campos <armandocamposf@gmail.com>
2025-12-16 18:00:23 -0400

    fix: add

--- src/prisma/prisma.servise.ts
+++ src/prisma/prisma.servise.ts
@@ -1,23 +1,10 @@
-// src/prisma/prisma.servise.ts - CÓDIGO CORREGIDO
+// src/prisma/prisma.servise.ts
 
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    constructor() {
-        super({
-            // SOLUCIÓN: Usar la estructura 'datasources'
-            datasources: {
-                db: {
-                    url: process.env.DATABASE_URL,
-                },
-            },
-            // Opcionalmente, puedes añadir logs, etc. aquí:
-            // log: ['query'],
-        });
-    }
-
     async onModuleInit() {
         await this.$connect()
     }
