c6a5897f0e75c46ea22b9bb82c451db17f7e89cb
Dev. Armando Campos <armandocamposf@gmail.com>
2025-12-29 16:40:33 -0400

    sdfsdf

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -776,7 +776,7 @@ export class TransactionService {
       orderBy: {
         publicId: 'desc'
       },
-      take: 15
+      take: 10
     });
 
     // Filtros de seguridad para los contadores (ignora filtros de búsqueda, respeta rol)
